a3afa8a577c040f1e8b255f127cc3cb6a3ac0002
Anjushekhawat1 <anjus5338@gmail.com>
2025-11-14 10:33:24 +0530

    Data type

---
+++ Day2.py
@@ -0,0 +1,57 @@
+#Data_types
+
+#1.String:
+s = "Anju Shekhawat"
+print(s)
+print(type(s))
+
+#2.Integer:
+i = 2
+print(i)
+print(type(i))
+
+#3.Float:
+f = 2.5
+print(f)
+print(type(f))
+
+#4.Complex:
+c = 1+3j
+print(c)
+print(type(c))
+
+#5.List:
+l = [1,2,3]
+print(l)
+print(l[0])
+print(l[1])
+print(l[2])
+print(type(l))
+
+#6.Tuple:
+t = (1,2,3)
+print(t)
+print(type(t))
+
+#7.Range:
+r = range(1,10)
+print(r)
+print(type(r))
+
+#8.Dictionary:
+d = {"name" : "Anju", "surname" : "Shekhawat"}
+print(d)
+print(type(d))
+
+#9.Set:
+s = {1,2,3}
+print(s)
+print(type(s))
+
+#10.Boolean:
+b = True
+o = False
+print(b)
+print(o)
+print(type(b))
+print(type(o))
