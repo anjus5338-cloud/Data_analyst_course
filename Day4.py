e49c908dd171b48541e0d841d671269ce5c9371e
Anjushekhawat1 <anjus5338@gmail.com>
2025-11-17 12:50:55 +0530

    Operators

---
+++ Day4.py
@@ -0,0 +1,85 @@
+# .replace()
+x = "helli"
+print(x.replace('i','o')) 
+
+# concatenation:
+x = "Hello"
+y = "World"
+print(x + " " + y)
+
+#String Format:
+fname = "Rahul"
+print(f"my name is:{fname}")
+
+#Escape character:
+x = "hello\\world"
+print(x)
+
+#Python boolean:
+print(4>5)
+print(4==5)
+print(4<5)
+
+#Operators:
+#1.Arithmetic operators:
+x = 25
+y = 5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
+
+#2.Comparison:
+x = 10
+y = 5
+print(x==y)
+print(x<y)
+print(x>y)
+print(x<=y)
+print(x>=y)
+print(x!=y)
+
+#Assignment operators:
+a1 = 5
+a1+=4
+print(a1)
+a1-=3
+print(a1)
+a1*=8
+print(a1)
+a1/=4
+print(a1)
+a1%=2
+print(a1)
+a1*=7
+print(a1)
+a1**=1
+print(a1)
+
+#Logical Operators:
+#AND:
+a1 = 10
+b1= 10
+print(a1==b1 and a1>b1)
+
+#OR:
+print(a1==b1 or a1>b1)
+
+#NOT:
+print(not(a1==b1))
+
+#Identifier:
+#in operator
+i = "Operators"
+j = "Examples of Operators"
+print(i in j)
+
+#not in operator
+print(i not in j)
+
+
+
+
