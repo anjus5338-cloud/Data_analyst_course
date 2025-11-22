b5c68985929a97886a419996fe2c14d3764320fc
Anjushekhawat1 <anjus5338@gmail.com>
2025-11-22 09:18:17 +0530

    List

---
+++ Day9.py
@@ -0,0 +1,39 @@
+#List:
+list = ["Ram","Shyam"]
+print(list)
+
+#insert()
+list = ["Ram","Shyam"]
+list.insert(1,"Rajiv")
+print(list)
+
+#append():
+list.append("Anil")
+print(list)
+
+#to update:
+list[0] = "Ansh"
+print(list)
+
+#extend():
+x = ["apple","banana","grapes"]
+y = ["peas","carrot"]
+x.extend(y)
+print(x)
+print(y)
+
+#Remove():
+x.remove("apple")
+print(x)
+
+#pop()
+x.pop(1)
+print(x)
+
+#clear()
+list.clear()
+print(list)
+
+#delete:
+del list
+print(list)
